e71cad15c5fdb672430b138c4fac3bf82cb672e8
FelipeValentim <felipevalentimdasilva@gmail.com>
2021-12-26 20:20:07 -0300

    v0.1

---
+++ C#/Projetos/NotePad/NotePad/FormPrincipal.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace NotePad
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonFileNew = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonFileImport = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButtonFileSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripComboBoxFonteTipo = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBoxFonteTamanho = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.richTextBox = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonFileNew,
+            this.toolStripSeparator1,
+            this.toolStripButtonFileImport,
+            this.toolStripSeparator2,
+            this.toolStripButtonFileSave,
+            this.toolStripSeparator3,
+            this.toolStripComboBoxFonteTipo,
+            this.toolStripComboBoxFonteTamanho,
+            this.toolStripSeparator4,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5,
+            this.toolStripSeparator5,
+            this.toolStripButton6,
+            this.toolStripButton7,
+            this.toolStripButton8});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(974, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButtonFileNew
+            // 
+            this.toolStripButtonFileNew.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonFileNew.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonFileNew.Image")));
+            this.toolStripButtonFileNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonFileNew.Name = "toolStripButtonFileNew";
+            this.toolStripButtonFileNew.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButtonFileNew.Text = "Novo arquivo";
+            this.toolStripButtonFileNew.Click += new System.EventHandler(this.toolStripButtonFileNew_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripButtonFileImport
+            // 
+            this.toolStripButtonFileImport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonFileImport.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonFileImport.Image")));
+            this.toolStripButtonFileImport.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonFileImport.Name = "toolStripButtonFileImport";
+            this.toolStripButtonFileImport.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButtonFileImport.Text = "Importar arquivo";
+            this.toolStripButtonFileImport.Click += new System.EventHandler(this.toolStripButtonFileImport_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripButtonFileSave
+            // 
+            this.toolStripButtonFileSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonFileSave.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonFileSave.Image")));
+            this.toolStripButtonFileSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonFileSave.Name = "toolStripButtonFileSave";
+            this.toolStripButtonFileSave.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButtonFileSave.Text = "Salvar arquivo";
+            this.toolStripButtonFileSave.Click += new System.EventHandler(this.toolStripButtonFileSave_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripComboBoxFonteTipo
+            // 
+            this.toolStripComboBoxFonteTipo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toolStripComboBoxFonteTipo.Items.AddRange(new object[] {
+            "Arial",
+            "Verdana",
+            "Bradley Hand ITC",
+            "Yu Gothic Light",
+            "Impact"});
+            this.toolStripComboBoxFonteTipo.Name = "toolStripComboBoxFonteTipo";
+            this.toolStripComboBoxFonteTipo.Size = new System.Drawing.Size(121, 27);
+            this.toolStripComboBoxFonteTipo.Text = "Arial";
+            this.toolStripComboBoxFonteTipo.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBoxFonteTipo_SelectedIndexChanged);
+            // 
+            // toolStripComboBoxFonteTamanho
+            // 
+            this.toolStripComboBoxFonteTamanho.AutoCompleteCustomSource.AddRange(new string[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "36",
+            "48",
+            "72"});
+            this.toolStripComboBoxFonteTamanho.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.toolStripComboBoxFonteTamanho.Items.AddRange(new object[] {
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "36",
+            "48",
+            "72"});
+            this.toolStripComboBoxFonteTamanho.Name = "toolStripComboBoxFonteTamanho";
+            this.toolStripComboBoxFonteTamanho.Size = new System.Drawing.Size(75, 27);
+            this.toolStripComboBoxFonteTamanho.Text = "10";
+            this.toolStripComboBoxFonteTamanho.SelectedIndexChanged += new System.EventHandler(this.toolStripComboBoxFonteTamanho_SelectedIndexChanged);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton5.Text = "toolStripButton5";
+            // 
+            // toolStripSeparator5
+            // 
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            this.toolStripSeparator5.Size = new System.Drawing.Size(6, 27);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton6.Text = "Alinhar para esquerda";
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton7.Text = "Centralizar";
+            // 
+            // toolStripButton8
+            // 
+            this.toolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
+            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton8.Name = "toolStripButton8";
+            this.toolStripButton8.Size = new System.Drawing.Size(24, 24);
+            this.toolStripButton8.Text = "Alinhar para direita";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(974, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // richTextBox
+            // 
+            this.richTextBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.richTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox.Location = new System.Drawing.Point(0, 27);
+            this.richTextBox.Name = "richTextBox";
+            this.richTextBox.Size = new System.Drawing.Size(974, 512);
+            this.richTextBox.TabIndex = 2;
+            this.richTextBox.Text = "";
+            this.richTextBox.SelectionChanged += new System.EventHandler(this.richTextBox_SelectionChanged);
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(974, 561);
+            this.Controls.Add(this.richTextBox);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPrincipal";
+            this.Text = "NotePad";
+            this.Load += new System.EventHandler(this.FormPrincipal_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonFileNew;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButtonFileImport;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton toolStripButtonFileSave;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxFonteTamanho;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxFonteTipo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+        private System.Windows.Forms.ToolStripButton toolStripButton8;
+        private System.Windows.Forms.RichTextBox richTextBox;
+    }
+}
+
---
+++ C#/Projetos/NotePad/NotePad/FormPrincipal.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotePad
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButtonFileNew_Click(object sender, EventArgs e)
+        {
+            //new FormPrincipal().Show();
+            richTextBox.Clear();
+
+            //this.Close();
+        }
+
+        private void toolStripButtonFileImport_Click(object sender, EventArgs e)
+        {
+            string[] linhas = { };
+            var filePath = string.Empty;
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            openFileDialog.Title = "Abrir arquivo de texto";
+            openFileDialog.Filter = "Arquivos de texto (*.txt)|*.txt|Todos os arquivos (*.*)|*.*";
+            if (openFileDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                filePath = openFileDialog.FileName;
+            }
+
+            if (filePath != string.Empty)
+            {
+                richTextBox.Clear();
+                richTextBox.Text = File.ReadAllText(filePath);
+                /*using (var streamReader = File.OpenText(filePath))
+                {
+                    linhas = streamReader.ReadToEnd().Split("\r\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    //textBox.Text += linhas;
+                }*/
+            }
+
+            /*foreach (string s in linhas)
+            {
+                textBox.Text += s + "\n";
+                
+            }*/
+        }
+
+        private void toolStripComboBoxFonteTamanho_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float fontSize = float.Parse(toolStripComboBoxFonteTamanho.Text);
+            //string fontSize = (toolStripComboBoxFonteTamanho.Text);
+            //textBox.Select
+            //textBox.Font = new Font(textBox.Font.FontFamily, fontSize);
+            //richTextBox.SelectionFont = 
+            richTextBox.SelectionFont = new Font(richTextBox.Font.FontFamily, fontSize);
+        }
+
+        private void toolStripComboBoxFonteTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float fontSize = richTextBox.SelectionFont.Size;
+            string fontStyle = toolStripComboBoxFonteTipo.Text;
+            richTextBox.SelectionFont = new Font(fontStyle, fontSize);
+        }
+
+        private void richTextBox_SelectionChanged(object sender, EventArgs e)
+        {
+            /*toolStripComboBoxFonteTamanho.Text = Convert.ToString(richTextBox.SelectionFont.Size);
+            toolStripComboBoxFonteTipo.Text = richTextBox.SelectionFont.Name;*/
+
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+            richTextBox.Font = new Font("Arial", 10);
+        }
+
+        private void toolStripButtonFileSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            //saveFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            saveFileDialog.Filter = "Arquivo de texto (*.txt)|*.txt";
+            saveFileDialog.Title = "Salvar arquivo de texto";
+            saveFileDialog.ShowDialog(this);
+
+            // If the file name is not an empty string open it for saving.
+            if (saveFileDialog.FileName != "")
+            {
+                File.WriteAllText(saveFileDialog.FileName, richTextBox.Text);
+            }
+        }
+    }
+}
